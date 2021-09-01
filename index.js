6039a232289d8e2926a0c996f6fe59578b02c092
SherzodbekAkhmadjonov <sherzodbekakhmadjonov2004@gmail.com>
2021-09-01 20:56:14 +0500

    Version 1.1.1

--- index.js
+++ index.js
@@ -1,37 +1,68 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>{
-  console.log("Succesfully connected");
-})
-.catch((err)=>{
-  console.log("Failed to connect", err);
-})
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Succesfully connected");
+  })
+  .catch((err) => {
+    console.log("Failed to connect", err);
+  });
 const sizeSchema = new mongoose.Schema({
   h: Number,
-  u:Number,
-  uom:String
-})
-const inventorySchema = new mongoose.Schema({
-  item:String,
-  qty:Number,
-  size:sizeSchema,
-  status:String
-}, {collection: 'inventory'})
-const InvetoryModel = mongoose.model('Inventory', inventorySchema)
-async function getItems1(){
+  u: Number,
+  uom: String,
+});
+const inventorySchema = new mongoose.Schema(
+  {
+    item: String,
+    qty: Number,
+    size: sizeSchema,
+    status: String,
+  },
+  { collection: "inventory" }
+);
+const InvetoryModel = mongoose.model("Inventory", inventorySchema);
+async function getItems1() {
   return await InvetoryModel.find({
-    status:"A"
+    status: "A",
   })
-  .sort({item:1})
-  .select({item:1, qty:1, _id:0})
+    .sort({ item: 1 })
+    .select({ item: 1, qty: 1, _id: 0 });
 }
-async function getItems2(){
-  return await InvetoryModel
-  .find()
-  .or([{qty: {$lte:30}}, { item: /.*a.*/i }])
-  .sort({qty: -1})
+async function getItems2() {
+  return await InvetoryModel.find()
+    .or([{ qty: { $lte: 30 } }, { item: /.*a.*/i }])
+    .sort({ qty: -1 });
 }
 async function run() {
   const selectedItem = await getItems2();
   console.log(selectedItem);
 }
-run()
+
+// 1th way to update item by only id
+
+async function updateItem1(id) {
+  const items = await InvetoryModel.findById(id);
+  if (!items) return;
+
+  items.item = "Desktop PC";
+
+  const updatedItem = await items.save();
+  console.log(updatedItem);
+}
+// 2nd way for updating by item filters
+async function updateItem2(id) {
+  const updatedItem = await InvetoryModel.updateOne(
+    { _id: id },
+    {
+      $set: {
+        item: "IMask",
+      },
+    }
+  );
+  console.log(updatedItem);
+}
+updateItem2("612920fefe2dc0b87af5cfc3");
